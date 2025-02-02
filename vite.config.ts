e76c1d4e16f2c7b847015e557939d9ec21b31570
rozsafa8 <istvan.kiss.dev@outlook.com>
2025-02-02 19:22:56 +0100

    build

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/rozsafa8/"
+  base: "/rozsafa8/",
+  build: {
+    outDir: "build",
+  }
 })
