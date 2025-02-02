f79f5f090c9c727eaddaced28ccd963cd32481a5
rozsafa8 <istvan.kiss.dev@outlook.com>
2025-02-02 20:03:41 +0100

    fix link

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
             <p>Közösségi oldal a Budapest XIII. kerületben, Rózsafa utca 8-10. alatt épülő GreenHome Rózsafa
                 társasház leendő lakói számára.</p>
             <p>Csatlakozz a társasház Facebook csoportjához, hogy értesülj a legfrissebb információkról!</p>
-            <a className="button" href="www.facebook.com/groups/rozsafa8">
+            <a className="button" href="https://www.facebook.com/groups/rozsafa8">
                 Csatlakozás a Facebook csoporthoz
             </a>
         </>
