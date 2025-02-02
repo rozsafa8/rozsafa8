d2f24fe1a814d01ebed22412935590714a077871
rozsafa8 <istvan.kiss.dev@outlook.com>
2025-02-02 18:54:34 +0100

    index

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h1>GreenHome Rózsafa Közösségi Oldal</h1>
+            <p>Közösségi oldal a Budapest XIII. kerületben, Rózsafa utca 8-10. alatt épülő GreenHome Rózsafa
+                társasház leendő lakói számára.</p>
+            <p>Csatlakozz a társasház Facebook csoportjához, hogy értesülj a legfrissebb információkról!</p>
+            <a className="button" href="https://www.facebook.com/groups/greenhomerozsafa" target="_blank">
+                Csatlakozás a Facebook csoporthoz
+            </a>
+        </>
+    )
 }
 
 export default App
